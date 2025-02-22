50811221ae5ed0e85a6a77e1527336da04a31072
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-22 13:19:00 +0700

    finished -4

---
+++ app/Models/Category.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Filament\Panel\Concerns\HasFont;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'is_expense',
+        'image'
+    ];
+}
