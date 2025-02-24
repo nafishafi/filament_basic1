799b17eae2f070145e6f980e00f818d61e13636b
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-24 11:28:11 +0700

    finished all

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -28,7 +28,7 @@ class TransactionResource extends Resource
                 Forms\Components\Select::make('category_id')
                     ->relationship('category', 'name')
                     ->required(),
-                Forms\Components\DatePicker::make('date')
+                Forms\Components\DatePicker::make('date_transaction')
                     ->required(),
                 Forms\Components\TextInput::make('amount')
                     ->required()
@@ -57,7 +57,7 @@ class TransactionResource extends Resource
                     ->trueColor('danger')
                     ->falseColor('success')
                     ->boolean(),
-                Tables\Columns\TextColumn::make('date')
+                Tables\Columns\TextColumn::make('date_transaction')
                     ->label('Tanggal')
                     ->sortable(),
                 Tables\Columns\TextColumn::make('amount')
--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,22 +2,33 @@
 
 namespace App\Filament\Widgets;
 
+use Illuminate\Support\Carbon;
 use App\Models\Transaction;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 
 class StatsOverview extends BaseWidget
 {
+    use InteractsWithPageFilters;
+    
     protected function getStats(): array
     {
-        $pemasukan = Transaction::incomes()->get()->sum('amount');
-        $pengeluaran = Transaction::expenses()->get()->sum('amount');
+        $startDate = ! is_null($this->filters['startDate'] ?? null) ?
+            Carbon::parse($this->filters['startDate']) :
+            null;
+
+        $endDate = ! is_null($this->filters['endDate'] ?? null) ?
+            Carbon::parse($this->filters['endDate']) :
+            now();
+
+        $pemasukan = Transaction::incomes()->whereBetween('date_transaction', [$startDate, $endDate])->sum('amount');
+        $pengeluaran = Transaction::expenses()->whereBetween('date_transaction', [$startDate, $endDate])->sum('amount');
         
         return [
-            Stat::make('Total Pemasukan', $pemasukan)
-                ->color('success'),
-            Stat::make('Total Pengeluaran', $pengeluaran),
-            Stat::make('Selisih', $pemasukan - $pengeluaran),
+            Stat::make('Total Pemasukan', 'Rp.'.' '. $pemasukan),
+            Stat::make('Total Pengeluaran',  'Rp.'.' '. $pengeluaran),
+            Stat::make('Selisih',  'Rp.'.' '. $pemasukan - $pengeluaran),
         ];
     }
 }
